d0712f89d358c5baf7a856aacf2fcf6562769576
Diptanu Baidya <129202457+Diptanu12@users.noreply.github.com>
2024-04-15 09:32:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your code here
+function countCharacters() {
+    var text = document.getElementById('evaluatedText').value;
+    var count = text.length;
+    document.getElementById('letterCount').textContent = count;
+}
